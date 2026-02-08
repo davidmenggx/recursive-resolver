a0c47621b7e57cd928bac1e9f152c9a49e63960e
David Meng <davidmenggx@gmail.com>
2026-02-07 18:00:28 -0800

    Cleaned up context class

--- src/resolver/context.py
+++ src/resolver/context.py
@@ -4,7 +4,7 @@ import struct
 import selectors
 from enum import Enum
 
-from protocol import DNSPacket
+from protocol import DNSPacket, DNSRecord
 
 class ProcessingState(Enum):
     PROCESSING_MESSAGE = 'PROCESSING_MESSAGE'
@@ -33,7 +33,6 @@ class Context:
         self.current_nameserver = '198.41.0.4'
         self.current_transaction_id = self.original_query.header.id
         self.depth = 0                      # Prevent infinite loops
-        self.visited_servers = set()        # Prevent infinite loops
 
         self.original_query.header.flags.rd = 0
 
@@ -68,51 +67,18 @@ class Context:
 
         self.process()
     
-    def resolve(self) -> None:
-        if self.depth > 10 or self.current_nameserver in self.visited_servers:
-            self.response = DNSPacket.create_simple_error(self.original_query.header.id, rcode=2)
-            self.finish_resolution()
-            print('ERROR! Maximum recursion depth reached!')
-
-        print('Resolving!')
-        # check the cache or forward to server
-        if self.response: 
-            if self.response.header.flags.rcode == 3: # UPDATE CACHE TOO
-                self.response = DNSPacket.create_simple_error(self.original_query.header.id, rcode=3)
-                self.finish_resolution()
-                print('NXDOMAIN')
-                return
-            
-            elif self.response.header.flags.rcode != 0:
-                self.response = DNSPacket.create_simple_error(self.original_query.header.id, rcode=2)
-                self.finish_resolution()
-                print('ERROR! Server failure!')
-                return
-
-            elif self.response.header.an_count > 0 or not self.client_wants_recursion:
-                self.finish_resolution()
-                print('FOUND!')
-                return
-            
-            elif self.response.header.ns_count > 0 and self.response.authorities:
-                name_server_found = False
-                for record in self.response.additionals:
-                    if record.type_ == 1:
-                        self.current_nameserver = record.rdata
-                        name_server_found = True
-                        break
-                if not name_server_found:
-                    self.response = DNSPacket.create_simple_error(self.original_query.header.id, rcode=2)
-                    self.send_response()
-                    print("COULDN'T FIND NEXT NAME SERVER")
-                    return
-            else:
-                self.response = DNSPacket.create_simple_error(self.original_query.header.id, rcode=2)
-                self.finish_resolution()
-                print('Server failure')
-                return
-        
+    def check_cache(self, ): # IMPORTANT: FILL THIS IN
+        ...
+    
+    def find_referral_ip(self, additional_records: list[DNSRecord]) -> str:
+        for record in additional_records:
+            if record.type_ == 1:
+                return record.rdata
+        return ''
+    
+    def send_request_upstream(self):
         upstream_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
         try:
             original_query = self.original_query.to_bytes()
         except (ValueError, struct.error, IndexError):
@@ -120,26 +86,54 @@ class Context:
             self.finish_resolution()
             print("Sent Format Error (RCODE 1) to client.")
             return
-
         try:
             upstream_socket.sendto(original_query, (self.current_nameserver, 53))
 
             self.sel.register(upstream_socket, selectors.EVENT_READ, self.handle_upstream_response)
 
             self.state = ProcessingState.WAITING_FOR_RESPONSE
-
             self.depth += 1
-            self.visited_servers.add(self.current_nameserver)
         except OSError:
             self.response = DNSPacket.create_simple_error(self.original_query.header.id, rcode=2)
             self.finish_resolution()
             print('Internal server error')
-        finally:
-            try:
-                upstream_socket.close()
-            except (NameError, AttributeError):
-                pass
+    
+    def resolve(self) -> None:
+        if self.depth > 10:
+            self.response = DNSPacket.create_simple_error(self.original_query.header.id, rcode=2)
+            self.finish_resolution()
+            print('ERROR! Maximum recursion depth reached!')
+
+        print('Resolving!')
+
+        self.check_cache()
+
+        if self.response:
+            if self.response.header.flags.rcode != 0:
+                if self.response.header.flags.rcode == 3:
+                    self.response = DNSPacket.create_simple_error(self.original_query.header.id, rcode=3)
+                    print('NXDOMAIN')
+                else:
+                    self.response = DNSPacket.create_simple_error(self.original_query.header.id, rcode=2)
+                    print('ERROR! Server failure!')
+                self.finish_resolution()
+                return
+
+            if self.response.header.an_count > 0 or not self.client_wants_recursion:
+                self.finish_resolution()
+                print('FOUND!')
+                return
             
+            if self.response.header.ns_count > 0 and self.response.authorities:
+                self.current_nameserver = self.find_referral_ip(self.response.additionals)
+                if not self.current_nameserver:
+                    self.response = DNSPacket.create_simple_error(self.original_query.header.id, rcode=2)
+                    self.finish_resolution()
+                    print("COULDN'T FIND NEXT NAME SERVER")
+                    return
+        
+        self.send_request_upstream()
+    
     def send_response(self) -> None:
         response = self.response
         if response:
