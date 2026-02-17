b9bee210324e76b0d20e8fa004142a69f88918eb
David Meng <davidmenggx@gmail.com>
2026-02-16 19:26:11 -0800

    Refactor to used NamedTuple for some more clarity

--- src/resolver/cache.py
+++ src/resolver/cache.py
@@ -1,8 +1,15 @@
+from __future__ import annotations
+
 import os
+from typing import NamedTuple
+
+class NodeEdge(NamedTuple):
+    edge: str
+    node: TrieNode
 
 class TrieNode:
     def __init__(self, is_leaf: bool = False):
-        self.children: dict[str, tuple[str, TrieNode]] = {}
+        self.children: dict[str, NodeEdge] = {}
         self.is_leaf: bool = is_leaf
 
 class RadixTrie:
@@ -19,29 +26,30 @@ class RadixTrie:
             match = curr.children.get(key[0])
 
             if not match: # add a new key
-                curr.children[key[0]] = (key, TrieNode(is_leaf=True))
+                curr.children[key[0]] = NodeEdge(key, TrieNode(is_leaf=True))
                 return
             
-            matching_edge, matching_node = match
+            matching_edge = match.edge
+            matching_node = match.node
 
             prefix = os.path.commonprefix([key, matching_edge])
             key = key[len(prefix):]
 
             if prefix == matching_edge: # full edge match => truncate and continue
-                curr = curr.children[prefix[0]][1]
+                curr = curr.children[prefix[0]].node
                 continue
             
             else:
                 intermediate_node = TrieNode()
-                intermediate_node.children[matching_edge[len(prefix):][0]] = (matching_edge[len(prefix):], matching_node)
+                intermediate_node.children[matching_edge[len(prefix):][0]] = NodeEdge(matching_edge[len(prefix):], matching_node)
 
                 del curr.children[matching_edge[0]]
-                curr.children[prefix[0]] = (prefix, intermediate_node)
+                curr.children[prefix[0]] = NodeEdge(prefix, intermediate_node)
 
                 if not key:
                     intermediate_node.is_leaf = True
                 else:
-                    intermediate_node.children[key[0]] = (key, TrieNode(is_leaf=True))
+                    intermediate_node.children[key[0]] = NodeEdge(key, TrieNode(is_leaf=True))
 
                 return
 
